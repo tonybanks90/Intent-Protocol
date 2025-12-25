50d6679433e7b6656e213e867c8d6564c3ad421d
tonybanks90 <tonybanks90@gmail.com>
2025-12-25 14:13:05 +0300

    Frontend Vite + React Init

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        proxy: {
+            '/api/bardock': {
+                target: 'https://aptos.testnet.bardock.movementlabs.xyz',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/bardock/, ''),
+                secure: true,
+            }
+        }
+    }
+})
